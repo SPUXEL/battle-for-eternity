3d15b6399e987e8ca640c8b48c13b907eb290217
Святослав Неверов <spuxel@yandex.ru>
2023-04-18 01:13:03 +0300

    Изменения в команде /start

--- battle-for-eternity/handlers/dm/base_commands.py
+++ battle-for-eternity/handlers/dm/base_commands.py
@@ -7,4 +7,16 @@ from data import langpack
 
 @dispatcher.message_handler(commands=["start"])
 async def command_start(update: Message):
-    await update.answer(langpack.START__MESSAGE)
+    await update.answer(
+        langpack.START__MESSAGE1.replace(
+            "user-name", update.from_user.full_name
+        )
+    )
+    await update.answer(
+        langpack.START__MESSAGE2,
+        disable_notification=True
+    )
+    await update.answer(
+        langpack.START__MESSAGE3,
+        disable_notification=True
+    )
